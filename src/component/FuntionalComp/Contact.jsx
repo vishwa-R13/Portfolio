57b602452c5ed588fb333c17252d766d44eef48e
vishwa <vishwarvish13@gmail.com>
2025-02-25 08:27:55 +0530

    change 1

--- src/component/FuntionalComp/Contact.jsx
+++ src/component/FuntionalComp/Contact.jsx
@@ -1,8 +1,33 @@
-function Contact(){
-    return(
-        <div>
-            <h1 className="sub-title">Contact</h1>
-        </div>
-    )
+function Contact() {
+  return (
+    <div>
+      <div >
+        <h1 className="sub-title">Contact Me</h1>
+        <hr />
+        <form>
+          <table>
+            <tr>
+              <td>
+                <label htmlFor="name">Name:</label>
+              </td>
+              <td>
+                <input type="text" placeholder=" Enter Your Name:" required />{" "}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="emil">Email:</label>
+              </td>
+              <td>
+                <input type="email" placeholder="Enter Your Email:" required />{" "}
+              </td>
+            </tr>
+            <button>Submit</button>
+          </table>
+        </form>
+      </div>
+
+    </div>
+  );
 }
 export default Contact;
